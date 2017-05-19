e0041ceaa57fa3a047622d99ed93dcadd96a10fc
rg-ci <ci@realglobe.jp>
2017-05-19 06:19:11 +0000

    [ci skip] Travis CI committed after build

---
+++ test/generateScopedName_test.js
@@ -0,0 +1,27 @@
+/**
+ * Test case for generateScopedName.
+ * Runs with mocha.
+ */
+'use strict'
+
+const generateScopedName = require('../lib/helpers/generateScopedName.js')
+const assert = require('assert')
+const co = require('co')
+
+describe('generate-scoped-name', function () {
+  this.timeout(3000)
+
+  before(() => co(function * () {
+
+  }))
+
+  after(() => co(function * () {
+
+  }))
+
+  it('Generate scoped name', () => co(function * () {
+
+  }))
+})
+
+/* global describe, before, after, it */
---
+++ test/processCss_test.js
@@ -0,0 +1,27 @@
+/**
+ * Test case for processCss.
+ * Runs with mocha.
+ */
+'use strict'
+
+const processCss = require('../lib/helpers/processCss.js')
+const assert = require('assert')
+const co = require('co')
+
+describe('process-css', function () {
+  this.timeout(3000)
+
+  before(() => co(function * () {
+
+  }))
+
+  after(() => co(function * () {
+
+  }))
+
+  it('Process css', () => co(function * () {
+
+  }))
+})
+
+/* global describe, before, after, it */
