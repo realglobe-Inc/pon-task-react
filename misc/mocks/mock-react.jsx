cd8a3952c49a291ef44cf8f957c44ed1ef1d8a31
okunishinishi <okunishinishi@gmail.com>
2017-09-08 10:00:10 +0900

    Watch css file to change

--- misc/mocks/mock-react.jsx
+++ misc/mocks/mock-react.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 import css from './mock-react.pcss'
 
 const SomeComponent = ({ id }) => (
-  <div className={css.hoge} id={id}>Hoge</div>
+  <div className={css.hoge} id={id}>HogeHoge</div>
 )
 
 export default SomeComponent
