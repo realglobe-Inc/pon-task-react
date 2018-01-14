720922d6bb1d722ae9cfe419489dd963c127093f
okunishinishi <okunishinishi@gmail.com>
2018-01-14 19:54:04 +0900

    use babel 7

--- misc/mocks/mock-obj.js
+++ misc/mocks/mock-obj.js
@@ -1,5 +1,19 @@
 'use strict'
 
-class Foo {
-  async sayYes () {}
+@annotation
+class MyClass {}
+
+function annotation (target) {
+  target.annotated = true
 }
+
+@isTestable(true)
+class MyClass2 {
+}
+
+function isTestable (value) {
+  return function decorator (target) {
+    target.isTestable = value
+  }
+}
+
