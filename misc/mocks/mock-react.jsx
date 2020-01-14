da5eefb22545d96fce502b85b905a943d2a53499
okunishinishi <okunishinishi@gmail.com>
2020-01-14 09:11:32 +0900

    update babel version

--- misc/mocks/mock-react.jsx
+++ misc/mocks/mock-react.jsx
@@ -11,6 +11,8 @@ const b = { ...a }
 
 console.log(a?.foo)
 
+console.log('hoge', false ?? 'x')
+
 void async function () {
   const { default: j } = await import('./mock-obj.js')
   console.log('dynamic j', j)
